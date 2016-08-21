3fc45dae78b8543a94d50417c3c5b148e1220a31
Noellie  B <noellieb@users.noreply.github.com>
2016-08-21 08:12:29 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ocstore/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ocstore/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/xampp/htdocs/ocstore/upload/catalog/');
+define('DIR_SYSTEM', 'E:/xampp/htdocs/ocstore/upload/system/');
+define('DIR_IMAGE', 'E:/xampp/htdocs/ocstore/upload/image/');
+define('DIR_LANGUAGE', 'E:/xampp/htdocs/ocstore/upload/catalog/language/');
+define('DIR_TEMPLATE', 'E:/xampp/htdocs/ocstore/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/xampp/htdocs/ocstore/upload/system/config/');
+define('DIR_CACHE', 'E:/xampp/htdocs/ocstore/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/xampp/htdocs/ocstore/upload/system/storage/download/');
+define('DIR_LOGS', 'E:/xampp/htdocs/ocstore/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/xampp/htdocs/ocstore/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/xampp/htdocs/ocstore/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'noellieb');
+define('DB_PASSWORD', 'Tell22Pass');
+define('DB_DATABASE', 'ocstore_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
